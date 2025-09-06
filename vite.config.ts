db5d32e5cae1a4498c64e07012318c4511536019
shuya-tamaru <tamaru1013@gmail.com>
2025-09-07 00:43:33 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { string } from "rollup-plugin-string";
 
 export default defineConfig({
-  base: "/particle-life/",
+  base: "/particle-life-wgsl/",
   plugins: [
     string({
       include: "**/*.wgsl",
